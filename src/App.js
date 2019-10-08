dc0eb348e4649388598ff850d83bf25acf8e9241
Gebruiker <pimk@hotmail.nl>
2019-10-08 11:11:15 +0200

    Base of our app with working Google Maps npm included

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Map, GoogleApiWrapper, Marker } from 'google-maps-react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+class App extends React.Component  {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      location: [
+        {latitude: 64.978134, longitude: 25.484502},
+        {latitude: 65.034004, longitude: 25.502414},
+        {latitude: 65.020885, longitude: 25.468680},
+        {latitude: 65.005526, longitude: 25.543921},
+        {latitude: 64.992263, longitude: 25.540666}
+      ]
+    }
+  }
+
+  showMarkers = () => {
+    return this.state.location.map((location, index) => {
+      return <Marker key={index} id={index} position={{
+       lat: location.latitude,
+       lng: location.longitude
+     }}
+     onClick={() => console.log("You clicked me!")} />
+    })
+  }
+
+  render() {
+    return (
+        <Map
+          google={this.props.google}
+          zoom={8}
+          initialCenter={{ lat: 65.016765, lng: 25.489747}}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          {this.showMarkers()}
+        </Map>
+    );
+  }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyD8bQqszeaK4TjSHJ_zpQqSQLwPGGhj4oc'
+})(App);
