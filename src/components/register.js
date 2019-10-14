aab8b9223e3d549f2da2f7a1d37c55cb68b5fea1
Gebruiker <pimk@hotmail.nl>
2019-10-14 15:59:53 +0200

    Added register form

---
+++ src/components/register.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+import createUser from "./createUser.js";
+
+export default function Register(props) {
+
+  function createUsers(event){
+    event.preventDefault();    
+    createUser.adding(event.target['username'].value, event.target['password'].value)
+      .then(result =>
+        {
+          console.log("succes");
+        })
+      .catch(() => {
+        console.log('Fail');
+      }) 
+  }
+
+    return (
+      <form method="POST" className="registerForm" onSubmit={createUsers}>
+        <input type="text" name="username" placeholder="Username" />
+        <input type="password" name="password" placeholder="Password" />
+        <Link to="/" className="lnk">Back</Link>
+        <button type="submit">Login</button>
+      </form>      
+    );
+}
